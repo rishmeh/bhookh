a2cd49fe2026600cc0af4a84c0ddc015a571590d
rishmeh <rishmehclass10@gmail.com>
2025-08-27 10:04:46 +0530

    removal of depracated features

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to database");
     } catch (err) {
         console.log("Database connection error:", err);
